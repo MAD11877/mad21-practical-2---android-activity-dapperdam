5bacad438abe32b3a4f474c4f217597db5cb86f9
dapperdam <71683772+dapperdam@users.noreply.github.com>
2022-05-29 14:48:37 +0800

    practical2 amendments

--- app/src/main/java/sg/edu/np/mad/s10206197practical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/s10206197practical2/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
+        Button button = findViewById(R.id.followbutton);
         User user = initUser();
         setFollowing(user,button);
         button.setOnClickListener(new View.OnClickListener() {
